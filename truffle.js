25300a93da08bd11898a6d957048cd9d9124c8f6
tran.minh.chien <SUN-ASTERISK\tran.minh.chien@b121716-pc.sun-asterisk.com>
2019-08-29 10:37:24 +0700

    setup mnemonic

--- truffle.js
+++ truffle.js
@@ -1,39 +1,35 @@
-'use strict'
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = 'below aware hen gap hole city tower announce vapor pumpkin steak either';
+'use strict';
+var HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: "8545",
-      network_id: "*", // Match any network id
+      host: '127.0.0.1',
+      port: '8545',
+      network_id: '*' // Match any network id
     },
     tomotestnet: {
-      provider: () => new HDWalletProvider(
-        mnemonic,
-        "https://testnet.tomochain.com",
-        0,
-        1,
-        true
-      ),
-      network_id: "89",
+      provider: () =>
+        new HDWalletProvider(process.env.MNENOMIC, 'https://testnet.tomochain.com', 0, 1, true),
+      network_id: '89',
       gas: 3000000,
       gasPrice: 20000000000000,
       gasLimit: 1000000
     },
     tomomainnet: {
-      provider: () => new HDWalletProvider(
-        mnemonic,
-        "https://rpc.tomochain.com",
-        0,
-        1,
-        true,
-        "m/44'/889'/0'/0/",
-      ),
-      network_id: "88",
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          'https://rpc.tomochain.com',
+          0,
+          1,
+          true,
+          "m/44'/889'/0'/0/"
+        ),
+      network_id: '88',
       gas: 2000000,
-      gasPrice: 10000000000000,
+      gasPrice: 10000000000000
     }
   }
 };
