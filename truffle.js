a4fc8ed0f80fc9214f3d94efbb0d2c79d109863d
hoang <vinhyenvodoi98@gmail.com>
2019-09-06 09:10:14 +0700

    add : solidity coverage

--- truffle.js
+++ truffle.js
@@ -30,6 +30,13 @@ module.exports = {
       network_id: '88',
       gas: 2000000,
       gasPrice: 10000000000000
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8555, // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01 // <-- Use this low gas price
     }
   }
 };
